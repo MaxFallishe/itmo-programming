c3bf83af4c94918c98d02ebb573a806c9516d099
Nick Borsov <n13b@inbox.ru>
2020-10-17 14:51:17 +0600

    Реализованы функции decrypt_caesar() и encrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,4 +1,5 @@
 import typing as tp
+import time
 
 
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
@@ -16,6 +17,34 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     ciphertext = ""
     # PUT YOUR CODE HERE
+    lower_letters_ords = [i for i in range(97, 122+1)]
+
+    for letter in plaintext:
+        upper_flag = False
+
+        if letter.isupper():
+            upper_flag = True
+
+        letter = letter.lower()
+
+        if ord(letter) in lower_letters_ords:
+
+            caesar_letter = chr(ord(letter)+shift)
+
+            if shift > 26:
+                shift = shift % 26
+
+            if ord(caesar_letter) not in lower_letters_ords:
+                caesar_letter = chr(min(lower_letters_ords) + (ord(caesar_letter) - max(lower_letters_ords))-1)
+
+            if upper_flag is True:
+                caesar_letter = chr(ord(caesar_letter)-32)
+
+            ciphertext += caesar_letter
+
+        else:
+            ciphertext += letter
+
     return ciphertext
 
 
@@ -34,6 +63,35 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
     plaintext = ""
     # PUT YOUR CODE HERE
+
+    lower_letters_ords = [i for i in range(97, 122 + 1)]
+
+    for letter in ciphertext:
+        upper_flag = False
+
+        if letter.isupper():
+            upper_flag = True
+
+        letter = letter.lower()
+
+        if ord(letter) in lower_letters_ords:
+
+            caesar_letter = chr(ord(letter) - shift)
+
+            if shift > 26:
+                shift = shift % 26
+
+            if ord(caesar_letter) not in lower_letters_ords:
+                caesar_letter = chr(max(lower_letters_ords) - (min(lower_letters_ords) - ord(caesar_letter)) + 1)
+
+            if upper_flag is True:
+                caesar_letter = chr(ord(caesar_letter) - 32)
+
+            plaintext += caesar_letter
+
+        else:
+            plaintext += letter
+
     return plaintext
 
 
