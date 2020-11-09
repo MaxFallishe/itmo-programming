f0fe13a42c4bbb872e951c841fb359067153bf1d
Nick Borsov <n13b@inbox.ru>
2020-11-09 13:54:41 +0600

    Удаление конструкции try except в функции solve и замена его на условие того содержит ли переменная pos значение None. Данная конструкция необходима чтобы прерывать рекурсию когда пустых не заполненных клеток в судоку не осталось.

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -251,21 +251,20 @@ def solve(grid: List[List[str]]) -> Optional[List[List[str]]]:  # type: ignore[r
     >>> solve(grid)
     [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
     """
-    try:
-        pos = find_empty_positions(grid)
-        possible_values = find_possible_values(grid, pos)  # type: ignore
-
-        if len(possible_values) == 0:
-            pass
-        else:
-            for possible_value in possible_values:
-                edited_grid = [i[:] for i in grid]
-                edited_grid[pos[0]][pos[1]] = possible_value  # type: ignore
-                answer = solve(edited_grid)
-                if answer is not None:
-                    return answer
-    except Exception as e:
+    pos = find_empty_positions(grid)
+    if pos is None:
         return grid
+    possible_values = find_possible_values(grid, pos)  # type: ignore
+
+    if len(possible_values) == 0:
+        pass
+    else:
+        for possible_value in possible_values:
+            edited_grid = [i[:] for i in grid]
+            edited_grid[pos[0]][pos[1]] = possible_value  # type: ignore
+            answer = solve(edited_grid)
+            if answer is not None:
+                return answer
 
 
 def check_solution(solution: List[List[str]]) -> bool:
