335b57607c737d2f0c27e342796984cf5faf8821
Nick Borsov <n13b@inbox.ru>
2020-11-14 03:06:01 +0600

    Реализация функционала life-console.py, спасибо Дмитрию Сергеевичу за пояснения <3

--- homework03/life-console.py
+++ homework03/life-console.py
@@ -10,13 +10,25 @@ class Console(UI):
 
     def draw_borders(self, screen) -> None:
         """ Отобразить рамку. """
-        pass
+        screen.border()
 
     def draw_grid(self, screen) -> None:
         """ Отобразить состояние клеток. """
-        pass
+        for row_num in range(len(self.life.curr_generation)):
+            for column_num in range(len(self.life.curr_generation[row_num])):
+                cell_status = self.life.curr_generation[row_num][column_num]
+                if cell_status == 1:
+                    screen.addch(row_num+1, column_num+1, '*')
+                elif cell_status == 0:
+                    screen.addch(row_num + 1, column_num + 1, ' ')
 
     def run(self) -> None:
         screen = curses.initscr()
-        # PUT YOUR CODE HERE
-        curses.endwin()
+        while True:
+            self.draw_borders(screen)
+            self.draw_grid(screen)
+            screen.refresh()
+            import time
+            time.sleep(1)
+            self.life.step()
+
