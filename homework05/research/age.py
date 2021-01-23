184fe46e532d61ffaf261349197fafea9b322074
Nick Borsov <n13b@inbox.ru>
2021-01-24 00:38:10 +0600

    Add blacked age.py

--- homework05/research/age.py
+++ homework05/research/age.py
@@ -14,4 +14,26 @@ def age_predict(user_id: int) -> tp.Optional[float]:
     :param user_id: Идентификатор пользователя.
     :return: Медианный возраст пользователя.
     """
-    pass
+    age = []
+    current_date = dt.date.today()
+    friends = get_friends(user_id, fields=["bdate"]).items
+    for friend in friends:
+        try:
+            birthday_date = dt.datetime.strptime(friend["bdate"], "%d.%m.%Y")  # type: ignore
+        except (KeyError, ValueError):
+            continue
+        age.append(
+            current_date.year
+            - birthday_date.year
+            - (
+                current_date.month < birthday_date.month
+                or (
+                    current_date.month == birthday_date.month
+                    and current_date.day < birthday_date.day
+                )
+            )
+        )
+
+    if age:
+        return statistics.median(age)
+    return None
