a4c945e10428b21b15118dd4c631583acec83c22
Nick Borsov <n13b@inbox.ru>
2021-01-24 01:02:05 +0600

    Add blacked wall.py

--- homework05/vkapi/wall.py
+++ homework05/vkapi/wall.py
@@ -2,12 +2,14 @@ import textwrap
 import time
 import typing as tp
 from string import Template
+import math
 
 import pandas as pd
 from pandas import json_normalize
 
 from vkapi import config, session
 from vkapi.exceptions import APIError
+from vkapi.config import VK_CONFIG
 
 
 def get_posts_2500(
@@ -20,7 +22,42 @@ def get_posts_2500(
     extended: int = 0,
     fields: tp.Optional[tp.List[str]] = None,
 ) -> tp.Dict[str, tp.Any]:
-    pass
+    idea = f"""
+       var i = 0; 
+           var result = [];
+               while (i < {max_count}){{
+                   if ({offset}+i+100 > {count}){{
+                       result.push(API.wall.get({{
+                       "owner_id": "{owner_id}",
+                       "domain": "{domain}",
+                       "offset": "{offset} +i",
+                       "count": "{count}-(i+{offset})",
+                       "filter": "{filter}",
+                       "extended": "{extended}",
+                       "fields": "{fields}"
+                    }}));
+               }} 
+               result.push(API.wall.get({{
+               "owner_id": "{owner_id}",
+               "domain": "{domain}",
+               "offset": "{offset} +i",
+               "count": "{count}",
+               "filter": "{filter}",
+               "extended": "{extended}",
+               "fields": "{fields}"
+               }}));
+               i = i + {max_count};
+           }}
+           return result;
+       """
+    data = {
+        "code": idea,
+        "access_token": VK_CONFIG["access_token"],
+        "v": VK_CONFIG["version"],
+    }
+    response = session.post("execute", data=data)
+    doc = response.json()
+    return doc["response"]["items"]
 
 
 def get_wall_execute(
@@ -49,4 +86,17 @@ def get_wall_execute(
     :param fields: Список дополнительных полей для профилей и сообществ, которые необходимо вернуть.
     :param progress: Callback для отображения прогресса.
     """
-    pass
+    user_posts = pd.DataFrame()
+    if progress is None:
+        progress = lambda x: x
+
+    for _ in progress(range(math.ceil(count / 2500))):
+        user_posts = user_posts.append(
+            json_normalize(
+                get_posts_2500(
+                    owner_id, domain, offset, count, max_count, filter, extended, fields
+                )
+            )
+        )
+        time.sleep(2)
+    return user_posts
