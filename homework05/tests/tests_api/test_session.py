a65b5272308b4b1e9fe9cc2ed610bff9378c3e97
Nick Borsov <n13b@inbox.ru>
2021-01-24 02:01:47 +0600

    add isort test_session.py

--- homework05/tests/tests_api/test_session.py
+++ homework05/tests/tests_api/test_session.py
@@ -5,6 +5,7 @@ import httpretty
 import responses
 from requests.exceptions import (ConnectionError, HTTPError, ReadTimeout,
                                  RetryError)
+
 from vkapi.session import Session
 
 
