a473bd2cbc9e3f40e86c34e4e6a97c70e04a829f
MaxFallishe <n13b@inbox.ru>
2021-10-11 22:42:24 +0300

    Making major edits in scraputils.py bayes.py hackernews.py

--- homework06/bayes.py
+++ homework06/bayes.py
@@ -1,17 +1,48 @@
+from collections import defaultdict
+from math import log
+
+
 class NaiveBayesClassifier:
 
-    def __init__(self, alpha):
-        pass
+    def __init__(self, a: int = 1e-5):
+        self.d = 0
+        self.word = defaultdict(lambda: 0)
+        self.classified_words = defaultdict(lambda: 0)
+        self.classes = defaultdict(lambda: 0)
+        self.a = a
 
-    def fit(self, X, y):
-        """ Fit Naive Bayes classifier according to X, y. """
-        pass
+    def fit(self, dataset, classes):
+        """ Fit Naive Bayes classifier according to titles, labels. """
 
-    def predict(self, X):
+        for i in range(len(dataset)):
+            self.classes[classes[i]] += 1
+            words = dataset[i].split()
+            for w in words:
+                self.word[w] += 1
+                self.classified_words[w, classes[i]] += 1
+
+        for c in self.classes:
+            self.classes[c] /= len(dataset)
+
+        self.d = len(self.word)
+
+    def predict(self, feature):
         """ Perform classification on an array of test vectors X. """
-        pass
+        assert len(self.classes) > 0
+        return max(
+            self.classes.keys(), key=lambda c:
+            log(self.classes[c]) + sum(
+                log((self.classified_words[w, c] + self.a) / (self.word[w] + self.a * self.d)
+                    ) for w in feature.split())
+        )
 
-    def score(self, X_test, y_test):
+    def _get_predictions(self, dataset):
+        classes = []
+        for feature in dataset:
+            classes.append(self.predict(feature))
+        return classes
+
+    def score(self, dataset, classes):
         """ Returns the mean accuracy on the given test data and labels. """
-        pass
-
+        predicted = self._get_predictions(dataset)
+        return sum(0 if predicted[i] != classes[i] else 1 for i in range(len(dataset))) / len(dataset)
--- homework06/hackernews.py
+++ homework06/hackernews.py
@@ -1,10 +1,9 @@
-from bottle import (
-    route, run, template, request, redirect
-)
+from bottle import route, run, template, request, redirect
+import string
 
-from scrapper import get_news
-from db import News, session
 from bayes import NaiveBayesClassifier
+from db import News, session
+from scraputils import get_news
 
 
 @route("/news")
@@ -16,21 +15,63 @@ def news_list():
 
 @route("/add_label/")
 def add_label():
-    # PUT YOUR CODE HERE
+    query = request.query.decode()
+    identifier = int(query['id'])
+    label = query['label']
+    s = session()
+    s.query(News.id).filter(News.id == identifier).update({News.label: label})
+    s.commit()
     redirect("/news")
+
+
+def has(ses, author, title):
+    return (len(ses.query(News.author).filter_by(author=author).all()) == 0 or
+            len(ses.query(News.title).filter_by(title=title).all()) == 0)
 
 
 @route("/update")
 def update_news():
-    # PUT YOUR CODE HERE
+    news = get_news('https://news.ycombinator.com/', 3)
+    s = session()
+    for n in news:
+        if has(s, n['author'], n['title']):
+            s.add(News(title=n['title'],
+                       author=n['author'],
+                       url=n['url'],
+                       points=n['points'],
+                       comments=n['comments']))
+    s.commit()
     redirect("/news")
+
+
+def clean(s):
+    translator = str.maketrans("", "", string.punctuation)
+    return s.translate(translator)
 
 
 @route("/classify")
 def classify_news():
-    # PUT YOUR CODE HERE
+    s = session()
+    model = NaiveBayesClassifier()
+    train_set = s.query(News).filter(News.label != None).all()
+    model.fit([clean(news.title).lower() for news in train_set], [news.label for news in train_set])
+    test = s.query(News).filter(News.label == None).all()
+    return template('news_template', rows=sorted(
+        test,
+        key=lambda news: get_weight(model.predict(clean(news.title).lower())))
+    )
+
+
+def get_weight(label):
+    if label == 'never':
+        return 2
+    elif label == 'maybe':
+        return 1
+    elif label == 'good':
+        return 0
+    else:
+        raise AssertionError("Invalid label" + label)
 
 
 if __name__ == "__main__":
     run(host="localhost", port=8080)
-
