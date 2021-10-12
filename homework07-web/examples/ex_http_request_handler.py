0c43eb0f6f16630c2dc13f92cc6bc9d555405667
MaxFallishe <n13b@inbox.ru>
2021-10-12 06:29:08 +0300

    Add teacher's examples

---
+++ homework07-web/examples/ex_http_request_handler.py
@@ -0,0 +1,12 @@
+from httpserver import BaseHTTPRequestHandler, HTTPServer
+
+
+def main():
+    server = HTTPServer(
+        timeout=10, port=5000, backlog_size=1, request_handler_cls=BaseHTTPRequestHandler
+    )
+    server.serve_forever()
+
+
+if __name__ == "__main__":
+    main()
